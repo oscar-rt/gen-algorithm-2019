8da61425d7da2b40aaf166dbf77c1d3a1453c322
unknown <oscarrtech@gmail.com>
2019-08-07 19:29:36 -0400

    added dist array

--- ga_main.py
+++ ga_main.py
@@ -11,7 +11,6 @@ import operator
 ###
 ###!CURRENT: Trying to figure out how to distribute child spots among cell hierarchy 
 
-
 #get random integer
 def g_ri():
     return random.randint(0, settings["MAX_NUMBER"])
@@ -44,24 +43,33 @@ class Cell:
             break
     #generate a cell from two other cells
     @classmethod
-    def from_reproduction(self, cell_parent_1, cell_parent_2):
+    def from_reproduction(cls, cell_parent_1, cell_parent_2):
+        #we naturally break from this loop if no zero division error occurs
         while True:
         #check if we're making a unique cell or not
             try:
                 if settings["GENE_MONITORING"] == "ON":
                     print("ON")#add functionality
-                else: 
+                else:
+                    cls = Cell() 
                     #declare the sequence
-                    self.sequence = []
+                    cls.sequence = []
                     #for every object in the sequence, choose which parent to inherit from at random
                     for strand in range(7):
-                        parent_rand = random.choice([cell_parent_1, cell_parent_2])
-                        self.sequence.append(parent_rand.sequence[strand])
+                        mutation_threshold = random.randint(1, 100)
+                        if mutation_threshold <= settings["MUTATION_RATE"] * 100:
+                            if strand % 2 == 0:
+                                cls.sequence.append(g_ri())
+                            else: 
+                                cls.sequence.append(g_ro())
+                        else:
+                            parent_rand = random.choice([cell_parent_1, cell_parent_2])
+                            cls.sequence.append(parent_rand.sequence[strand])
                     #same as randomly generating a cell
-                    self.number = eval(''.join(str(e) for e in self.sequence)) #join sequence into string and evaluate the expression 
-                    self.emargin = abs(settings["TARGET_NUMBER"] - self.number) #error margin of the evaluated sequence
+                    cls.number = eval(''.join(str(e) for e in cls.sequence)) #join sequence into string and evaluate the expression 
+                    cls.emargin = abs(settings["TARGET_NUMBER"] - cls.number) #error margin of the evaluated sequence
                     #return a pointer to the object
-                    return self
+                    return cls
             except ZeroDivisionError:
                 continue
             break
@@ -72,8 +80,8 @@ class Cell:
 #important variables for main loop
 generations = 0
 reached_target = False
-deaths = settings["DEATH_RATE"] * settings["MAX_POPULATION"] if settings["DEATH_RATE"] * settings["MAX_POPULATION"] <= settings["MAX_POPULATION"] - 1 else settings["MAX_POPULATION"] - 1
-child_spots = settings["MAX_POPULATION"] -  deaths
+deaths = int(settings["DEATH_RATE"] * settings["MAX_POPULATION"] if settings["DEATH_RATE"] * settings["MAX_POPULATION"] <= settings["MAX_POPULATION"] - 1 else settings["MAX_POPULATION"] - 1)
+child_spots = int(settings["MAX_POPULATION"] -  deaths)
 rep_dict = {}
 #################################################
 #inital setup                                   #
@@ -89,7 +97,48 @@ def update_rd():
     for num, cell in enumerate(cell_pool):
         pass
 
+#get dynamic fitness-based distribution array of all cells in the pool
+def get_distribution_array():
+    
+    d_array = []
 
+    #get all emargins from the cell pool and find biggest e_margin (which should be last one anyway)
+    l_emargin = 0
+    for cell in cell_pool:
+        d_array.append(cell.emargin)
+        if cell.emargin > l_emargin:
+            l_emargin = cell.emargin
+    
+    #turn all emargins into fitness maxes and get the sum of the fitness maxes
+    t_sum = 0
+    for i, d in enumerate(d_array):
+        d_array[i] = (d - l_emargin) * -1
+        t_sum = t_sum + d_array[i]
+    
+    #normalize fitness maxes in proportion to child spots
+    
+    for i, d in enumerate(d_array):
+        d_array[i] = (d / t_sum) * child_spots
+        
+
+    #move surplus (decimal numbers) to next value, essentially distributing the children
+    surplus = 0
+    for i, d in reversed(list(enumerate(d_array))):
+        
+        d_array[i] = d_array[i] + surplus
+        lcopy = float(d_array[i])
+        d_array[i] = int(d_array[i])
+
+        surplus = lcopy - int(lcopy)
+
+    return d_array
+
+mcell = Cell()
+print(mcell)
+fcell = Cell()
+print(fcell)
+scell = Cell().from_reproduction(mcell, fcell)
+print(scell)
 
 #main loop
 while generations <= settings["MAX_GENERATIONS"] or not reached_target:
@@ -104,7 +153,7 @@ while generations <= settings["MAX_GENERATIONS"] or not reached_target:
     update_rd()
 
     #repopulate up until the max cell count, figure out how to divide the child spots among the sorted cell pool
-    
+    distribution_array = get_distribution_array()
 
     #check if any cell has reached the target fitness, if so, stop evolution
     for cell in cell_pool:
--- ga_setup.py
+++ ga_setup.py
@@ -8,9 +8,9 @@ settings = {
     "MAX_GENERATIONS" : 10000,
     #the maximum number of cells, anywhere from 1 to 17297280
     "MAX_POPULATION" : 100,
-    #what percentage of the population dies
+    #what percentage of the population dies 0-1
     "DEATH_RATE" : .5,
-    #the rate at which a mutation can occur
+    #the rate at which a mutation can occur 0-1
     "MUTATION_RATE" : .01,
     #maximum integer generated, upper bound is inclusive, should be equal to or less than target
     "MAX_NUMBER" : 100,
